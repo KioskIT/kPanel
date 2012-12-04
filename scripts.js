a1df55278fcfea1c292d3fb2caded05cc4bcc278
Victor Cristian Popescu <victorcrpopescu@gmail.com>
2012-12-04 00:51:38 +0000

    Now using Aptana Studio (test page)

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function selector(selectedVersion)
+{
+	deselectEverything();
+	
+	selectedVersion.style.border="3px solid #268a16";
+}
+
+function deselectEverything()
+{
+	document.getElementById("version1").style.border="1px solid #268a16";
+	document.getElementById("version2").style.border="1px solid #268a16";
+	document.getElementById("version3").style.border="1px solid #268a16";
+	document.getElementById("version4").style.border="1px solid #268a16";
+	document.getElementById("version5").style.border="1px solid #268a16";
+	document.getElementById("version6").style.border="1px solid #268a16";
+	document.getElementById("version7").style.border="1px solid #268a16";
+	document.getElementById("version8").style.border="1px solid #268a16";
+	document.getElementById("version9").style.border="1px solid #268a16";
+	document.getElementById("version10").style.border="1px solid #268a16";
+	document.getElementById("version11").style.border="1px solid #268a16";
+	document.getElementById("version12").style.border="1px solid #268a16";
+	document.getElementById("version13").style.border="1px solid #268a16";
+	document.getElementById("version14").style.border="1px solid #268a16";
+	document.getElementById("version15").style.border="1px solid #268a16";
+}
