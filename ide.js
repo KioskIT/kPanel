0ad26972eb04296a1e952e962d0196795450e1d0
Victor Cristian Popescu <victorcrpopescu@gmail.com>
2012-12-08 14:15:02 +0000

    Fixed canvas size and positioning

--- ide.js
+++ ide.js
@@ -1,11 +1,62 @@
 var canvas, context, width, height;
 
+var elements = [];
+
 function initalizeCanvas()
 {
     canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");
-    width = canvas.innerWidth;
-    height = canvas.innerHeight;
+    
+    var ratio = 1.7778;
+    
+    var viewportWidth = window.innerWidth;
+    var viewportHeight = window.innerHeight;
+
+    width = viewportWidth - 300 - 150 - 100;
+    height = width / ratio;
+    
+    canvas.style.position = "absolute";
+    canvas.setAttribute("width", width);
+    canvas.setAttribute("height", height);
+    
+    canvas.style.top = ((viewportHeight - height) / 2) + "px";
+    canvas.style.left = "200px";
+    
+    canvas.addEventListener('click', 
+        function(event) 
+        {
+            var x = event.offsetX;
+            var y = event.offsetY;
+            
+            var found_element = false;
+            
+            for (var i = 0; i < elements.length; ++i)
+            {
+               if (y > elements[i].top && y < elements[i].top + elements[i].height && x > elements[i].left && x < elements[i].left + elements[i].width) 
+                {
+                    select(i);
+                    found_element = true;
+                    break;
+                }                
+            }    
+            
+            if (found_element == false)
+            {
+                deselect();
+            }
+        }, 
+    false);
+}
+
+function select(index)
+{
+    context.fillStyle = "rgba(0, 0, 0, 0.1)";
+    context.fillRect(elements[index].top, elements[index].left, elements[index].top + elements[index].width, elements[index].left + elements[index].height);
+}
+
+function deselect()
+{
+    
 }
 
 function addText()
@@ -14,7 +65,13 @@ function addText()
     context.fillStyle = "black";
     context.textBaseline = "top";
     context.textAlign = "left";
-    context.fillText("Sample text", 250, 250);
+    context.fillText("Sample text", 50, 50);
+    
+    elements.push({
+        width: 100,
+        height: 100,
+        top: 50,
+        left: 50});
 }
 
 function addImage()
@@ -46,4 +103,3 @@ function addGallery()
 {
     // Button disabled    
 }
-
